f7631fffa08a2888f98356a6b34a5bc0cdcbe306
Bartosz Bazański <bazanski.bartosz@gmail.com>
2024-07-07 21:44:26 +0200

    fix: use html img tag

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -54,12 +54,10 @@ const Navigation = () => {
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                   <div className="flex flex-shrink-0 items-center">
                     <Link href="/">
-                      <Image
+                      <img
                         className="h-8 w-auto"
-                        src="https://placehold.co/32x32"
+                        src="https://placehold.co/32x32.png"
                         alt="Your Company"
-                        width={32}
-                        height={32}
                       />
                     </Link>
                   </div>
