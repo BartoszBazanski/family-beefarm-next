879add537c32f94dc145fecf3a1745d9d120a848
Bartosz Bazański <bazanski.bartosz@gmail.com>
2024-07-07 21:16:16 +0200

    fix: add Suspense component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 'use client';
 
+import { Suspense } from 'react';
 import { Inter } from 'next/font/google';
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import classNames from 'classnames';
 
 import Footer from '@/components/Footer';
 import Navigation from '@/components/Navigation';
 
 import './globals.css';
-import classNames from 'classnames';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,17 +20,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={classNames(inter.className)}>
-        <ProgressBar
-          height="4px"
-          color="#F2994A"
-          options={{ showSpinner: false }}
-          shallowRouting
-        />
-        <Navigation />
-        <main className="min-h-screen pt-16">
-          <div className="container mx-auto p-5">{children}</div>
-        </main>
-        <Footer />
+        <Suspense>
+          <ProgressBar
+            height="4px"
+            color="#F2994A"
+            options={{ showSpinner: false }}
+            shallowRouting
+          />
+          <Navigation />
+          <main className="min-h-screen pt-16">
+            <div className="container mx-auto p-5">{children}</div>
+          </main>
+          <Footer />
+        </Suspense>
       </body>
     </html>
   );
