2e304f852e3f1b515bd00e68fb9dd169acc7ef2d
Bartosz Bazański <bazanski.bartosz@gmail.com>
2025-03-31 20:23:15 +0200

    feat: add additional image to honey card

--- src/app/miody/page.tsx
+++ src/app/miody/page.tsx
@@ -5,6 +5,8 @@ import { HoneyCard } from '@/components/HoneyCard/HoneyCard';
 import GoldenrodHoneyJarImage from '@/assets/images/honey/honey_goldenrod_jar.png';
 import HeatherHoneyJarImage from '@/assets/images/honey/honey_heather_jar.png';
 import HoneydewHoneyJarImage from '@/assets/images/honey/honey_honeydew_jar.png';
+import GoldenrodImage from '@/assets/images/honey/goldenrod.png';
+import HeatherImage from '@/assets/images/honey/heather.png';
 
 export const metadata: Metadata = {
   title: 'Nasze miody | Pasieka Szczygiełkowa',
@@ -15,9 +17,24 @@ export default function HoneysPage() {
     <section className="text-primary container min-h-screen pt-24 pb-10">
       <h2 className="mb-10 text-3xl font-medium md:text-4xl">Nasze miody</h2>
       <div className="mb-10 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        <HoneyCard imageSrc={GoldenrodHoneyJarImage} title="Miód nawłociowy" linkHref="/miody/nawlociowy" />
-        <HoneyCard imageSrc={HeatherHoneyJarImage} title="Miód wrzosowy" linkHref="/miody/wrzosowy" />
-        <HoneyCard imageSrc={HoneydewHoneyJarImage} title="Miód spadziowy" linkHref="/miody/spadziowy" />
+        <HoneyCard
+          imageSrc={GoldenrodHoneyJarImage}
+          additionalImageSrc={GoldenrodImage}
+          title="Miód nawłociowy"
+          linkHref="/miody/nawlociowy"
+        />
+        <HoneyCard
+          imageSrc={HeatherHoneyJarImage}
+          additionalImageSrc={HeatherImage}
+          title="Miód wrzosowy"
+          linkHref="/miody/wrzosowy"
+        />
+        <HoneyCard
+          imageSrc={HoneydewHoneyJarImage}
+          additionalImageSrc={GoldenrodImage}
+          title="Miód spadziowy"
+          linkHref="/miody/spadziowy"
+        />
       </div>
     </section>
   );
---
+++ src/components/HexFrame/HexFrameImage.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx';
+import Image, { StaticImageData } from 'next/image';
+
+import { ClassNameProp } from 'typings/props';
+
+type HexFrameImageProps = {
+  imageSrc: StaticImageData;
+} & ClassNameProp;
+
+export const HexFrameImage = ({ className, imageSrc }: HexFrameImageProps) => {
+  return (
+    <div
+      className={clsx(
+        'from-secondary/90 bg-clip-hexagon flex size-32 items-center justify-center bg-radial to-70%',
+        className,
+      )}
+    >
+      <div className="bg-secondary bg-clip-hexagon h-25 w-22 p-px">
+        <div className="bg-secondary bg-clip-hexagon size-full p-0.5">
+          <div className="bg-secondary bg-clip-hexagon size-full p-px">
+            <Image src={imageSrc} alt="Hex frame" className="bg-clip-hexagon size-full object-cover" width={100} height={100} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
